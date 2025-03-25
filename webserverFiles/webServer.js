8d1b8498415753917f8cf211f13558b63833e57d
[NEO] NitroBuilderGTR <57569616+NitroBuilderGTR@users.noreply.github.com>
2025-03-25 07:41:03 +0000

    Add files via upload

---
+++ webserverFiles/webServer.js
@@ -0,0 +1,31 @@
+var portNumber = 3579; // Changed to 8080 (or any other free port)
+var express = require("express");
+var app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get("/", function (request, response) {
+    response.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
+
+app.use('/css', express.static('public/css'));
+app.use('/js', express.static('public/js'));
+app.use('/png', express.static('public/images'));
+
+app.listen(portNumber, (err) => {
+    if (err) {
+        console.error("Error starting server:", err);
+    } else {
+        console.log(`Opticol Web Server | [STATUS: ONLINE]`);
+        console.log(`Active Link: http://localhost:${portNumber}`)
+    }
+});
+
+app.on('error', (err) => {
+    if (err.code === 'EADDRINUSE') {
+        console.error(`Port ${portNumber} is already in use. Choose another port.`);
+    } else {
+        console.error('Server error:', err);
+    }
+});
